85a65751e7b81078c54cd137a1b92fb4c0c2314e
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-01 18:40:59 +0600

    Create increDercreOperator.php

---
+++ Operators/increDercreOperator.php
@@ -0,0 +1,31 @@
+<?php
+
+    $counter = 10;
+    $counter = $counter + 1;
+    echo $counter . PHP_EOL;
+
+    //++
+    //post increment
+    echo $counter++ . PHP_EOL;
+    echo $counter . PHP_EOL;
+
+    //pre increment operator
+    echo ++$counter . PHP_EOL;
+    echo $counter . PHP_EOL;
+
+    //-- operator
+    $counter = 10;
+    $counter  -= 1;
+    echo $counter . PHP_EOL;
+
+    //++
+    //post increment
+    echo $counter-- . PHP_EOL;
+    echo $counter . PHP_EOL;
+
+    //pre increment operator
+    echo --$counter . PHP_EOL;
+    echo $counter . PHP_EOL;
+
+
+?>
