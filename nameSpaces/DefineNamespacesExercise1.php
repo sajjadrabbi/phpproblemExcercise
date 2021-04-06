6f17a45cc7cee8c27638136f744be6fff20c3d4b
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-06 12:20:36 +0600

    namespaces

---
+++ nameSpaces/DefineNamespacesExercise1.php
@@ -0,0 +1,16 @@
+<?php
+    //include the file of the namespace
+    include "MyNamespace.php";
+
+
+    //calling
+    echo onlyConstant\NAME;
+    echo "his age is: " . onlyConstant\AGE;
+    echo "his gender is: " . onlyConstant\GENDER;
+
+
+
+
+
+
+?>
---
+++ nameSpaces/MyNamespace.php
@@ -0,0 +1,10 @@
+<?php
+    //defining the namespace name as a folder for the constant
+    namespace onlyConstant;
+
+    //assigning the constant
+    const NAME = "Rabbi is learning PHP <br>" .PHP_EOL;
+    const AGE = __NAMESPACE__ . 26 . "<br>" . PHP_EOL;
+    const GENDER = __NAMESPACE__ . " male" . PHP_EOL;
+
+?>
