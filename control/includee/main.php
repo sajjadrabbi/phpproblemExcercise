dc00b4be3dc9be5dc5c360a5d2c4365e5249b69b
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-07 13:19:13 +0600

    include

---
+++ control/includee/main.php
@@ -0,0 +1,15 @@
+<?php
+    include "head.php";
+?>
+
+<body>
+
+    <?php
+        include "menu.php";
+    ?>
+
+    <h1>Excercise on how to include files</h1>
+
+<?php
+    include "footer.htm";
+?>
