6e0bc72c04c7166730218398eaeaf4cf4fc1ee69
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-07 13:26:07 +0600

    include and require

--- control/includeeAndRequire/main.php
+++ control/includeeAndRequire/main.php
@@ -6,6 +6,8 @@
 
     <?php
         include "menu.php";
+        // require "submenu.php"; this keyword is used 
+        //when we dont want to execute further if the file is missing
     ?>
 
     <h1>Excercise on how to include files</h1>
