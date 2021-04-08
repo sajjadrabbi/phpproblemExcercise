79c54296006d95ec2d101dde28302be3b1856f1a
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-08 10:32:28 +0600

    Create combinestring.php

---
+++ strings/combinestring.php
@@ -0,0 +1,18 @@
+<?php
+
+    $firstName = "Rabbi";
+    $lastName = ", SAjjAd";
+    $fullName = $firstName . $lastName;
+    echo $fullName . PHP_EOL;
+
+    $mark = 100;
+    $passed = true;
+
+    $content = "student $fullName got total " . $mark . " he is 
+    passed - " . $passed;
+
+    echo $content;
+
+
+
+?>
