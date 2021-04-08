9a450c37ad95a8baf5c6b5252bfda70618ea926a
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-08 11:17:00 +0600

    Create stringLength.php

---
+++ strings/stringLength.php
@@ -0,0 +1,22 @@
+<?php
+    $name = "Rabbi";
+    echo strlen($name) . PHP_EOL;
+
+    echo strlen($name1) . PHP_EOL;
+
+    $name = "";
+    echo strlen($name) .PHP_EOL;
+    
+    $name = null;
+    echo strlen($name) . PHP_EOL;
+
+    $name = "something";
+    if(strlen($name) > 0){
+        echo $name .PHP_EOL;
+    }else{
+        echo "$name is empty" . PHP_EOL;
+    }
+
+
+
+?>
