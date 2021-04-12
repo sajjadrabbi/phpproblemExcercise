d45f8fc6c77f626df282f94739c761d02f7336de
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-12 11:13:09 +0600

    Create RaiseCatachException.php

---
+++ exception/RaiseCatachException.php
@@ -0,0 +1,41 @@
+<?php
+
+    //funtion of divide
+    function divide($x, $y):int {
+
+        if($y <= 0){
+            //when the error is found throw the message to catch block
+            throw new Exception("Divide by zero exception might happen");
+        }
+
+        $result = $x / $y;
+        return $result;
+    }
+
+    try{
+        //executing the code which might give error
+        //here i am runnning my code and also checking for errors
+        $result = divide(8,0);
+        echo $result;
+
+    }catch(Exception $e){
+        //from the throw keyword, catch is accepts the exception
+        echo "Caught Exception: " . $e->getmessage() .PHP_EOL;
+    }
+
+    //now i can execute other line without the eroor getting in the way
+    //or not letting the other get executed
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
