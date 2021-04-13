04126f3b86f0a8ea3b4cb654586a37227687e116
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-13 16:22:59 +0600

    file

--- files/ReadFile.php
+++ files/ReadFile.php
@@ -1,7 +1,15 @@
 <?php
 
-    
+    file_put_contents("NewFile.txt", "some more new text\r\n", FILE_APPEND);
 
+    $fileHandler = fopen("NewFile.txt", "r");
+    $fileSize = filesize("NewFile.txt");
+
+    $content = fread($fileHandler, $fileSize);
+
+    echo $content . PHP_EOL;
+
+    fclose($fileHandler);
 
 
 
