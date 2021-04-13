29d520f6200da2d168ccb6e57206c9f24074f89b
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-13 18:49:17 +0600

    Create TimeDiff.php

---
+++ date/TimeDiff.php
@@ -0,0 +1,22 @@
+<?php
+
+    $date1 = time();
+    sleep(10);
+    $date2 = time();
+    $diff = ($date2 - $date1) / 60;
+    echo $diff;
+
+    $start = date_create('2019-05-26 12:01:00');
+    $end = date_create("2019-05-26 13:15:00");
+    $diff = date_diff($end, $start);
+    print_r($diff);
+
+
+
+
+
+
+
+
+
+?>
