c16023abf2dda40a7d60636fe7ce8eb4bbb13048
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-16 12:54:55 +0600

    Update Bank.php

--- Oop/Bank.php
+++ Oop/Bank.php
@@ -15,12 +15,20 @@
         }
 
 
-        function deductAmount($amt) : int{
+        function deductAmount($amt){
 
+            if($this->balance <=0){
+                echo "No balance in the Account" . PHP_EOL;
+                return;
+            }
+
+            if($this->balance <= $amt){
+                echo "Requested Amount is greater than Balance." . PHP_EOL;
+                return;
+            }
+    
             $this->balance -= $amt;
-            return $this->balance; 
         }
-
 
         function checkBalance(){
             echo "The balance is $this->balance" . PHP_EOL;
@@ -33,6 +41,8 @@
     $user1->accNo = 101;
     $user1->name = "Rabbi";
     $user1->depositAmount(50);
+    $user1->deductAmount(5000);
+    echo $user1->name . " ID is $user1->accNo and has bank balance $user1->balance." .PHP_EOL;
     $user1->checkBalance();
 
 
