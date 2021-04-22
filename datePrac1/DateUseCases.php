0dac7982524d26bb90c1243d99d9289d45706bee
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-22 14:31:48 +0600

    Create DateUseCases.php

---
+++ datePrac1/DateUseCases.php
@@ -0,0 +1,14 @@
+<?php
+
+echo date_create("now")->format("Y-m-d H:i:s") . PHP_EOL;
+echo date_create("+1 day")->format("Y-m-d H:i:s") . PHP_EOL;
+echo date_create("-1 day")->format("Y-m-d H:i:s") . PHP_EOL;
+
+$strTime =  strtotime("2019-5-21 9:00:00"); // it is converted to milisec
+echo date("d:m:y h:i:s", $strTime) . PHP_EOL;
+
+if (time() >= $strTime) {
+    echo "Time is ahead";
+} else {
+    echo "Time is not ahead";
+}
