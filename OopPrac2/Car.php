e1b6eb31a9efb4d06207fcf99a5bfe7dac789651
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-23 09:23:50 +0600

    Update Car.php

--- OopPrac2/Car.php
+++ OopPrac2/Car.php
@@ -16,5 +16,10 @@ $car1 = new Car();
 $car1->name = "Ferrrari";
 $car1->color = "Red";
 $car1->sayHello();
-
 echo "$car1->name is the big car and its color is $car1->color." . PHP_EOL;
+
+$car2 = new Car();
+$car2->name = "Ferrrari";
+$car2->color = "Red";
+$car2->sayHello();
+echo "$car2->name is the big car and its color is $car2->color." . PHP_EOL;
