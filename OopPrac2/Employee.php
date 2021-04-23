d558b888f4ae7f28c68bdd8fdcae6a1bb27f2dda
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-23 13:36:58 +0600

    com

--- OopPrac2/Employee.php
+++ OopPrac2/Employee.php
@@ -1,11 +1,7 @@
 <?php
 
-// Calculate the employee salary
-
 class Employee
 {
-
-    //Properties of the employee
     public $id;
     public $name;
     public $workingHoursPerDay = 8;
@@ -13,34 +9,27 @@ class Employee
     public $totalLeaveTaken;
     public $workingDays;
 
-    //Methods of the class
     function getSalaryAmount($totalDays): int
     {
-        //this is a special variable which can be used to  access th class property 
+        //$this is a special variable which can be used to acess class properties
         $this->workingDays = $totalDays - $this->totalLeaveTaken;
-
         $salary = $this->workingDays * $this->workingHoursPerDay * $this->hourlyRate;
-
         return $salary;
     }
 }
 
-$emp1 = new Employee();
+$emp1 = new Employee(); //created a instance of an object
 $emp1->id = 101;
-$emp1->name = "John, Smith";
+$emp1->name = "John Smith";
 $emp1->totalLeaveTaken = 4;
 $salary = $emp1->getSalaryAmount(20);
-
-echo $emp1->name . "has worked for $emp1->workingDays and taken $emp1->totalLeaveTaken leaves" . PHP_EOL;
+echo "$emp1->name has worked for $emp1->workingDays and taken $emp1->totalLeaveTaken leaves." . PHP_EOL;
 echo "$emp1->name salary is $salary" . PHP_EOL;
 
-
-//secod employeee------------------    
-$emp2 = new Employee();
-$emp2->id = 102;
-$emp2->name = "Rabbi";
-$emp2->totalLeaveTaken = 2;
+$emp2 = new Employee(); //created a instance of an object
+$emp2->id = 201;
+$emp2->name = "Ronal Komen";
+$emp2->totalLeaveTaken = 5;
 $salary = $emp2->getSalaryAmount(20);
-
-echo $emp2->name . " has worked for $emp2->workingDays and taken $emp2->totalLeaveTaken leaves" . PHP_EOL;
+echo "$emp2->name has worked for $emp2->workingDays and taken $emp2->totalLeaveTaken leaves." . PHP_EOL;
 echo "$emp2->name salary is $salary" . PHP_EOL;
