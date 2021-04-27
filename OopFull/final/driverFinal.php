d899b1fd48a682ccbd2ca3b065ac15ed77add8a5
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-04-27 10:30:04 +0600

    Create driverFinal.php

---
+++ OopFull/final/driverFinal.php
@@ -0,0 +1,17 @@
+<?php
+
+final class DriverFinal
+{
+
+    private $updateInstruction = "instruct";
+
+    final function runDriverUpdate()
+    {
+        echo 'executing the update instruction' . PHP_EOL;
+    }
+}
+
+//class WindowProgram extends DriverFinal
+//{
+    //cannot be extended
+//}
