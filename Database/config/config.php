1f21198be664efb929ce126b9f655c90f7b4fb48
sajjadrabbi <47458709+sajjadrabbi@users.noreply.github.com>
2021-05-08 14:28:54 +0600

    done

---
+++ Database/config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root"); //this user has all the previlage to access the database
+define("DB_PWD", "mysql");
+define("DB_NAME", "db_oop");
